79464823910f457ea873149f937bf00b40bdc8fa
Luca D'Angelo <infolucadangelo.it>
2018-07-07 16:52:51 +0100

    -

---
+++ 1.2 Check Permutation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._2_Check_Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
