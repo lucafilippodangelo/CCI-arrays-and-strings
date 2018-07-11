989161472cd84e878d5822488bc092ad56b1dc97
Luca D'Angelo <infolucadangelo.it>
2018-07-11 08:06:46 +0100

    -

--- 1.4 Palindrome and Permutation/Program.cs
+++ 1.4 Palindrome and Permutation/Program.cs
@@ -4,9 +4,33 @@ namespace _1._4_Palindrome_and_Permutation
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            String[] strings = {"Rats live on no evil star",
+                            "A man, a plan, a canal, panama",
+                            "Lleve",
+                            "Tacotac",
+                            "asda"};
+
+            foreach (String s in strings)
+            {
+                bool a = Implementation.IsPermutationOfPalindrome(s);
+                bool b = Implementation.IsPermutationOfPalindrome2(s);
+                bool c = Implementation.IsPermutationOfPalindrome3(s);
+                Console.WriteLine(s);
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Agree: " + a);
+                }
+                else
+                {
+                    Console.WriteLine("Disagree: " + a + ", " + b + ", " + c);
+                }
+                Console.WriteLine();
+            
         }
+    }
     }
 }
